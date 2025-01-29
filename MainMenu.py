19e2001dbd1bb50c36c9251458123544677e6cbd
RebelPilot89 <marcocrush@gmail.com>
2025-01-28 21:01:34 -0600

    Update MainMenu.py
    change to python3

--- MainMenu.py
+++ MainMenu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
-#Github     : http://github.com/mishakorzik                                                                           
+# Copyright 2021 AllHackingTools
+# Written by : Misha Korzhik
+# Github     : http://github.com/mishakorzik                                                                           
 import os
 import time
 import sys
@@ -15,87 +15,87 @@ op = str(input("Options: "))
 if op == "1":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/IpMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/IpMenu.py")
 elif op == "2":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/RouterMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/RouterMenu.py")
 elif op == "3":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/MailMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/MailMenu.py")
 elif op == "4":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/WebMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/WebMenu.py")
 elif op == "5":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/CamHackMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/CamHackMenu.py")
 elif op == "6":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/AndroidMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/AndroidMenu.py")
 elif op == "7":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/SQLinjectionMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/SQLinjectionMenu.py")
 elif op == "8":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/SocialMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/SocialMenu.py")
 elif op == "9":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/SpamMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/SpamMenu.py")
 elif op == "10":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/AnalistickMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/AnalistickMenu.py")
 elif op == "11":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/DarkSearchMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/DarkSearchMenu.py")
 elif op == "12":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/PhishingMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/PhishingMenu.py")
 elif op == "13":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/PassworldMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/PassworldMenu.py")
 elif op == "14":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/WordlistGeneratorMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/WordlistGeneratorMenu.py")
 elif op == "15":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/XSSAttackMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/XSSAttackMenu.py")
 elif op == "16":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/discordMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/discordMenu.py")
 elif op == "17":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/telegramMenu.py")
+ os.system("cd && cd AllHackingTools && python3 Files/telegramMenu.py")
 elif op == "18":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/Other.py")
+ os.system("cd && cd AllHackingTools && python3 Files/Other.py")
 elif op == "19":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/TermuxS.py")
+ os.system("cd && cd AllHackingTools && python3 Files/TermuxS.py")
 elif op == "20":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 .settings/settingsMenu.py")
+ os.system("cd && cd AllHackingTools && python3 .settings/settingsMenu.py")
 elif op == "21":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("clear && cd && cd AllHackingTools && bash .settings/LICENSE.sh && cd && cd AllHackingTools && python3 src/Timer2.py && python2 MainMenu.py")
+ os.system("clear && cd && cd AllHackingTools && bash .settings/LICENSE.sh && cd && cd AllHackingTools && python3 src/Timer2.py && python3 MainMenu.py")
 elif op == "22":
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
@@ -115,11 +115,4 @@ elif op == "13324715":
  print("[DEBUG] Warning! Customization has been disabled.")
 elif op == "24":
  os.system("clear && cd && cd AllHackingTools && bash Logo.sh")
- print("\033[1;31;40mExiting System...")
- time.sleep(0.7)
-else:
- print("\033[1;31;40mInvalid input. Reloading Tools")
- time.sleep(1.6)
- os.system("cd")
- os.system("cd AllHackingTool")
- os.system("python2 MainMenu.py")
+ print("\033[_{{{CITATION{{{_1{](https://github.com/mishakorzik/AllHackingTools/tree/33b4c49b2995e09853328e52d9694b0a9f317cab/.settings%2FDeveloperMenu.py)[_{{{CITATION{{{_2{](https://github.com/mishakorzik/AllHackingTools/tree/33b4c49b2995e09853328e52d9694b0a9f317cab/.settings%2Fdesing%2FMainMenu.py)[_{{{CITATION{{{_3{](https://github.com/mishakorzik/AllHackingTools/tree/33b4c49b2995e09853328e52d9694b0a9f317cab/MainMenu.py)
